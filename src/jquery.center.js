5361e90823840200d89c1eeb31ff66a268365cb6
Juho Vepsalainen <bebraw@gmail.com>
2014-04-17 12:10:08 +0300

    Set up 'use strict';

--- src/jquery.center.js
+++ src/jquery.center.js
@@ -1,15 +1,17 @@
 (function ($) {
+    'use strict';
+
     // http://stackoverflow.com/a/210733/228885
     $.fn.center = function ($parent) {
         $parent = $parent || $(window);
 
         var $e = this;
 
-        this.css("position","absolute");
-        this.css("top", Math.max(0, (($parent.height() - this.outerHeight()) / 2) +
-                                 $(window).scrollTop()) + "px");
-        this.css("left", Math.max(0, (($parent.width() - this.outerWidth()) / 2) +
-                                  $(window).scrollLeft()) + "px");
+        this.css('position', 'absolute');
+        this.css('top', Math.max(0, (($parent.height() - this.outerHeight()) / 2) +
+                                 $(window).scrollTop()) + 'px');
+        this.css('left', Math.max(0, (($parent.width() - this.outerWidth()) / 2) +
+                                  $(window).scrollLeft()) + 'px');
 
         if(!this.data('centered')) {
             $(window).on('resize', center).on('scroll', center);
